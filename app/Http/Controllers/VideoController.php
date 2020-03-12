15e7e9c156f9d2c5c3823af37afed44211399ed4
qing <17600969308@163.com>
2020-03-12 11:13:26 +0800

    video

--- app/Http/Controllers/VideoController.php
+++ app/Http/Controllers/VideoController.php
@@ -19,6 +19,7 @@ class VideoController extends Controller
     //视频列表
     public function video_list(){
         $category_id = Input::get('category_id');
+
         if (empty($category_id)){
             $wx_list = DB::table('videos')->where('type' , 1)->get(['id' , 'video_name' , 'image' , 'watch_number']);
             $cp_list = DB::table('videos')->where('type' , 2)->get(['id' , 'video_name' , 'image' , 'watch_number']);
@@ -27,9 +28,16 @@ class VideoController extends Controller
             $cp_list = DB::table('videos')->where(array('vc_id'=>$category_id , 'type'=>2))->get(['id' , 'video_name' , 'image' , 'watch_number']);
         }
 
+        //维修视频列表
+        $wx_list = $this->image_url($wx_list , '2' , 'image');
+        //产品讲解列表
+        $cp_list = $this->image_url($cp_list , '2' , 'image');
         $list['wx_list'] = $wx_list;
         $list['cp_list'] = $cp_list;
-        $data = $this->image_url($list , '2' , 'image');
-        return $this->returnAjax($data , '获取成功' , 200);
+        //主推视频列表
+        $main_video = DB::table('video_main')->get(['data_id' , 'image']);
+        $main_video = $this->image_url($main_video , '2' , 'image');
+        $list['main_video'] = $main_video;
+        return $this->returnAjax($list , '获取成功' , 200);
     }
 }
