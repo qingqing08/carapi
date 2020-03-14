1fb91044c66dbf372563a56e0100ad6e3c7472e9
qing <17600969308@163.com>
2020-03-14 15:57:38 +0800

    反馈

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -58,9 +58,7 @@ class IndexController extends Controller
         $data = file_get_contents('php://input');
 
         $arr = json_decode($data , true);
-//        print_r($arr);
-//        return $this->returnAjax($arr , '123' , 200);
-        $arr['email'] = '1515212@qq';
+
         $preg_email='/^[a-zA-Z0-9]+([-_.][a-zA-Z0-9]+)*@([a-zA-Z0-9]+[-.])+([a-z]{2,5})$/ims';
         if(!preg_match($preg_email , $arr['email'])){
             return $this->returnAjax('' , '邮箱地址不正确' , 100);
